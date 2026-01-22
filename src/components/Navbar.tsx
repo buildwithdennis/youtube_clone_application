36cf10621a5e43495b59b7824c38c4626a390097
dennis_kofi_smith <dennissmithkofi@gmail.com>
2026-01-22 15:34:46 +0000

    done day......................

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,13 @@
 import { Stack } from '@mui/material'
 import { Link } from 'react-router-dom'
+
+
+
 const Navbar = () => {
   return (
-    <div> Navbar Component </div>
+    <Stack
+      direction="row"
+      overflow="auto"/>
   )
 }
 
