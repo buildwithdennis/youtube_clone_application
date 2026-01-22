5ab5a27168516c026dc572fe60d52b8935c1091f
dennis_kofi_smith <dennissmithkofi@gmail.com>
2026-01-22 16:34:05 +0000

    end event

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,20 @@
+
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import  { Paper, IconButton } from '@mui/material'
+
+
+function SearchBar() {
+
+  return (
+    <Paper
+    component="form"
+    onSubmit={() => {}}
+
+    >
+
+    </Paper>
+  )
+}
+
+export default SearchBar
